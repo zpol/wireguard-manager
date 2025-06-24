396769cdbf8b3b3ea079ecf85b94b3bff9fab52c
Pol <poljane007@gmail.com>
2025-06-24 16:31:53 +0200

    fix pkt fowding

--- backend/main.go
+++ backend/main.go
@@ -389,8 +389,8 @@ func generateServerConfig(server models.Server) (string, error) {
 	config.WriteString(fmt.Sprintf("Address = %s\n", server.Address))
 	config.WriteString(fmt.Sprintf("ListenPort = %d\n", server.ListenPort))
 	config.WriteString(fmt.Sprintf("PrivateKey = %s\n", server.PrivateKey))
-	config.WriteString("PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth+ -j MASQUERADE\n")
-	config.WriteString("PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth+ -j MASQUERADE\n")
+	config.WriteString("PostUp = sysctl -w net.ipv4.ip_forward=1; iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE\n")
+	config.WriteString("PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE\n")
 	config.WriteString("\n")
 
 	for _, peer := range server.Peers {
